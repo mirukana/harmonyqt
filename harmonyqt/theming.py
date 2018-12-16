4b2ea12040d770e5a640e4f8ad1e0f8598521454
miruka <miruka@disroot.org>
2018-12-16 08:45:13 -0400

    Cache all theme/icon files on start
    Avoid hitting the disk for random actions that require loading icons or
    other stuff later

--- harmonyqt/theming.py
+++ harmonyqt/theming.py
@@ -2,8 +2,8 @@
 # This file is part of harmonyqt, licensed under GPLv3.
 
 import abc
-import os
-from functools import lru_cache
+from pathlib import Path
+from typing import Any, Dict, List, Sequence, Tuple
 
 from pkg_resources import resource_filename
 # pylint: disable=no-name-in-module
@@ -15,56 +15,84 @@ from .__about__ import __doc__
 _PKG = __about__.__name__
 
 
-class Resource(abc.ABC):
+class ResourceDir(abc.ABC):
     def __init__(self, base_dir: str, res_name: str) -> None:
-        self.name:      str       = res_name
-        self.dir:       str       = f"{base_dir}/{res_name}"
-        self.base_path: str       = resource_filename(_PKG, self.dir)
+        self.name:      str  = res_name
+        self.dir:       str  = f"{base_dir}/{res_name}"
+        self.base_path: Path = Path(resource_filename(_PKG, self.dir))
+
+        # {relative path: {ext: (path, file content as bytes)}}
+        self._cache: Dict[str, Dict[str, Tuple[Path, Any]]] = {}
+        self.reload()
 
 
-    def get_resource_path(self, filename: str, exts: tuple,
-                          relative: bool = False) -> str:
-        not_found = []
+    def reload(self) -> None:
+        self._cache_dir(self.base_path)
+
+
+    def _cache_dir(self, path: Path) -> None:
+        for item in path.iterdir():
+            if item.is_dir():
+                self._cache_dir(item)
+            if item.is_file():
+                self._cache_file(item)
+
+
+    def _cache_file(self, path: Path) -> None:
+        key, ext = "/".join(path.relative_to(self.base_path).parts)\
+                   .split(".", maxsplit=1)
+
+        file_dict      = self._cache.setdefault(key, {})
+        file_dict[ext] = (path, self.cache_file_content(path))
+
+
+    @abc.abstractmethod
+    def cache_file_content(self, path: Path) -> Any:
+        pass
+
+
+    def get_resource(self, relative_path: str, exts: Sequence[str]
+                    ) -> Tuple[Path, Any]:
+        not_found: List[Path] = []
 
         for ext in exts:
-            rel_path = f"{self.dir}/{filename}.{ext}"
-            path     = resource_filename(_PKG, rel_path)
-            if os.path.exists(path):
-                return rel_path if relative else path
-
-            not_found.append(path)
+            try:
+                return self._cache[relative_path][ext]
+            except KeyError:
+                not_found.append(self.base_path / f"{relative_path}.{ext}")
 
         obj   = type(self).__name__.lower()
-        files = "\n".join((f"  {f}" for f in not_found))
+        files = "\n".join((f"  {p!s}" for p in not_found))
         raise FileNotFoundError(
             f"None of these files found for {obj} {self.name!r}:\n{files}"
         )
 
 
-    @lru_cache(128)
-    def get_resource_content(self, filename: str, exts: tuple) -> str:
-        with open(self.get_resource_path(filename, exts), "r") as file:
-            return file.read()
-
-
-class Theme(Resource):
+class Theme(ResourceDir):
     def __init__(self, name: str) -> None:
         super().__init__("themes", name)
 
 
-    def style(self, filename: str) -> str:
-        return self.get_resource_content(filename, ("qss", "css"))
+    def cache_file_content(self, path: Path) -> str:
+        return path.read_text()
 
 
-class Icons(Resource):
+    def style(self, relative_path: str) -> str:
+        return self.get_resource(relative_path, ("qss", "css"))[1]
+
+
+class Icons(ResourceDir):
     def __init__(self, name: str) -> None:
         super().__init__("icons", name)
 
 
-    def path(self, filename: str, relative: bool = False) -> str:
-        return self.get_resource_path(filename, ("png",), relative)
+    def cache_file_content(self, path: Path) -> QIcon:
+        return QIcon(str(path))
 
 
-    @lru_cache(128)
-    def icon(self, filename: str) -> QIcon:
-        return QIcon(self.path(filename))
+    def path(self, relative_path: str) -> str:
+        return str(self.get_resource(relative_path, ("png",))[0])
+
+
+    def icon(self, relative_path: str) -> str:
+        return self.get_resource(relative_path, ("png",))[1]
